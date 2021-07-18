2d73a61e757ad1a163a3cf7294b05b352e0063ad
Bajiken <bajikenofficial@gmail.com>
2021-07-17 20:58:10 -0700

    Gps test

--- robot/gps.lua
+++ robot/gps.lua
@@ -2,6 +2,9 @@ local gpsRobot = {}
 
 function gpsRobot.getGpsCoordinates()
   local x, y, z = gps.locate()
+  print(x)
+  print(y)
+  print(z)
   while x == nil or y == nil or z == nil do
     x, y, z = gps.locate()
   end
