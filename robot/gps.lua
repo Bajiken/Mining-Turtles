be3c5a81a720e4959d1308e8051ba6811e5816ac
Bajiken <bajikenofficial@gmail.com>
2021-07-17 20:34:23 -0700

    Fix robot gps

--- robot/gps.lua
+++ robot/gps.lua
@@ -1,6 +1,6 @@
-local gps = {}
+local gpsRobot = {}
 
-function gps.getGpsCoordinates()
+function gpsRobot.getGpsCoordinates()
   local x, y, z = gps.locate()
   while x == nil or y == nil or z == nil do
     x, y, z = gps.locate()
@@ -8,4 +8,4 @@ function gps.getGpsCoordinates()
   return x, y, z
 end
 
-return gps
+return gpsRobot
