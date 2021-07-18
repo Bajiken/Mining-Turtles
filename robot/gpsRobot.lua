fcc316966acb937cf9b33452178ffe61c4e9652f
Bajiken <bajikenofficial@gmail.com>
2021-07-17 21:11:31 -0700

    Reset GPS one more time

--- robot/gpsRobot.lua
+++ robot/gpsRobot.lua
@@ -1,6 +1,6 @@
 local gpsRobot = {}
 
-function gpsRobot.getGpsCoordinates(gps)
+function gpsRobot.getGpsCoordinates()
   local x, y, z = gps.locate()
   while x == nil or y == nil or z == nil do
     x, y, z = gps.locate()
