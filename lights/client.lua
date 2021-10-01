1da3ea766247682296d530020e765abee267865e
Bajiken <bajikenofficial@gmail.com>
2021-09-30 19:42:54 -0700

    Remove duplicate split function

--- lights/client.lua
+++ lights/client.lua
@@ -89,17 +89,6 @@ function split (inputstr, sep)
     return t
 end
 
-function split (inputstr, sep)
-    if sep == nil then
-            sep = "%s"
-    end
-    local t={}
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-            table.insert(t, str)
-    end
-    return t
-end
-
 local config = fs.open("config", "r")
 local track = config.readLine()
 local gitPrefix = "https://raw.githubusercontent.com/Bajiken/Mining-Turtles/mainline/"
