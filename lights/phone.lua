6194e8f874e6bdb962f2bb86aac1f783f2659d10
Bajiken <bajikenofficial@gmail.com>
2021-10-14 23:26:06 -0700

    Add initial phone app for light show

--- lights/phone.lua
+++ lights/phone.lua
@@ -6,8 +6,23 @@ local PHONE_PORT = 2
 
 arg[1] = arg[1] or 0
 
-local payloadMessage = string.format("%d",
-    arg[1]
-)
+local interval = .095
+local counter = 0
+local bars = 8
+local totalNotes = bars * 8 * 4
+local payloadMessage = "go"
+local drift = .05 - math.fmod(.095, .05)
+local totalDrift = 0
 
-modem.transmit(CLIENT_PORT, PHONE_PORT, payloadMessage)
+while counter < totalNotes do
+  modem.transmit(CLIENT_PORT, PHONE_PORT, payloadMessage)
+  totalDrift = totalDrift + drift
+  if totalDrift >= .05 then
+    os.sleep(interval - .05)
+    totalDrift = totalDrift - .05
+  else
+    os.sleep(interval)
+  end
+  counter = counter + 1
+
+end
